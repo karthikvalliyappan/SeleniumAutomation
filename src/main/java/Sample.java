9f304d48be0804c9cd36e740291e88e98682493e
karthikvalliyappan <reachvkarthiksai@gmail.com>
2025-04-14 16:22:01 +0530

    Third commit

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -3,5 +3,6 @@ public class Sample
     public static void main(String[] args)
     {
         System.out.println("Hello world");
+        System.out.println("this is the sample second commit");
     }
 }
